6df58e6bce3da08a564973376f5c17f1bb24e1cc
alexplayer15 <alexplayer15@icloud.com>
2024-05-10 14:25:24 +0100

    updated pylint

--- auth.py
+++ auth.py
@@ -1,17 +1,27 @@
-from flask import Flask, request, jsonify, redirect, render_template, session, Blueprint
-from validation import validate_firstname, validate_lastname, validate_username, validate_password, validate_email, validate_re_password
-from flask_sqlalchemy import SQLAlchemy
-from werkzeug.security import generate_password_hash, check_password_hash
-from flask_mail import Mail, Message
+"""
+This module ...
+"""
 import secrets
+from flask import request, redirect, render_template, session, Blueprint
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_mail import Message
+from validation import \
+    validate_firstname, \
+    validate_lastname, \
+    validate_username, \
+    validate_password, \
+    validate_email, \
+    validate_re_password
+
 from extensions import db, mail
 from models import User
 
 auth_bp = Blueprint('auth', __name__)
 
-users = {}
-
 def send_email(subject, recipients, body):
+    """
+    This function creates an email object. 
+    """
     msg = Message(subject, recipients=recipients)
     msg.body = body
     mail.send(msg)
@@ -19,28 +29,28 @@ def send_email(subject, recipients, body):
 @auth_bp.route("/signin", methods = ['GET', 'POST'])
 
 def signin():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
+    """
+    This function handles the user signing in by taking in the username and password inputs. 
+    """
+    if request.method=='POST':
+        username=request.form['username']
+        password=request.form['password']
 
-        user = User.query.filter_by(username=username).first()
+        user=User.query.filter_by(username=username).first()
 
         if user and check_password_hash(user.password, password):
-        
-            session['username'] = username
+            session['username']=username
             return redirect('/success')
-    
-        else:
-            return redirect('/signin')
-        
+
     return render_template('sign_in.html')
 
-@auth_bp.route("/signup", methods = ['GET', 'POST'])
-
+@auth_bp.route("/signup", methods=['GET', 'POST'])
 def signup():
-
+    """
+    This function handles the user signing up by taking in basic user inputs, validating them. 
+    """
     print('This bit happened')
-    if request.method == 'POST':
+    if request.method=='POST':
         firstname = request.form['firstname']
         print('firstname =', firstname)
         lastname = request.form['lastname']
@@ -53,65 +63,78 @@ def signup():
         password_not_hash = request.form['password']
         print('password_not_hash =', password_not_hash)
         print('password =', password)
-        rePassword = request.form['re-password']
-        print('re-password =', rePassword)
+        re_password = request.form['re-password']
+        print('re-password =', re_password)
 
-        existing_user = User.query.filter_by(username=username).first()
-        existing_email = User.query.filter_by(email=email).first()
+        existing_user=User.query.filter_by(username=username).first()
+        existing_email=User.query.filter_by(email=email).first()
+
+        all_verified = (validate_firstname(firstname)
+        and validate_lastname(lastname)
+        and validate_password(password)
+        and validate_username(username)
+        and validate_email(email)
+        and validate_re_password(re_password,password_not_hash))
 
         if existing_user or existing_email:
             print('User or email already exists')
             return render_template('sign_up.html')
-        
-        
-        if (validate_firstname(firstname) 
-        and validate_lastname(lastname) 
-        and validate_password(password) 
-        and validate_username(username) 
-        and validate_email(email) 
-        and validate_re_password(rePassword,password_not_hash)):
-            print('This bit happened 3') 
+        if all_verified:
+            print('This bit happened 3')
 
+            verification_token=secrets.token_urlsafe(16)
 
-            verification_token = secrets.token_urlsafe(16) 
-            users[email] = {'verified': False, 'verification_token': verification_token}
+            new_user = User(firstname=firstname,
+                            lastname=lastname,
+                            username=username,
+                            password=password,
+                            email=email,
+                            token=verification_token)
 
-            new_user = User(firstname=firstname, lastname=lastname, username=username, password=password,email=email,token=verification_token)
             db.session.add(new_user)
             db.session.commit()
 
-            send_email('Welcome to Our App', [email], f'Thank you for signing up! Here is your verification link: http://127.0.0.1:5000/verify/{verification_token}')
+            send_email('Welcome to Our App', [email],
+            f'Thank you for signing up!/n' 
+            f'Here is your verification link: http://127.0.0.1:5000/verify/{verification_token}')
 
-            session['username'] = username
-            session['firstname'] = firstname
-            session['lastname'] = lastname
-            session['password'] = password
-            session['email'] = email
+            session['username']=username
+            session['firstname']=firstname
+            session['lastname']=lastname
+            session['password']=password
+            session['email']=email
 
             return redirect('/email')
-             
-        else:
-            return render_template('sign_up.html')
     else:
         print('This is happening')
 
     return render_template('sign_up.html')
 
 @auth_bp.route('/email')
-def email():
-
+def email_func():
+    """
+    This function displays the email page users are directed to after a successful sign-up. 
+    """
     return render_template('email_page.html')
 
 @auth_bp.route('/verify/<token>', methods=['GET'])
 def verify(token):
-    
-    for email, user in users.items():
-        if user['verification_token'] == token:
-            user['verified'] = True
-            return redirect('/signin')
+    """
+    This function verifies the email token. 
+    """
+    user = User.query.filter_by(token=token).first()
+
+    if user:
+        user.verified = True
+        db.session.commit()
+        return redirect('/signin')
+
     return 'Invalid verification token'
 
 @auth_bp.route('/success')
 def success():
+    """
+    This function displays the home page users are directed to after a successful sign-in. 
+    """
 
     return render_template('home.html')
--- main.py
+++ main.py
@@ -1,30 +1,43 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+"""
+This module ...
+"""
 import os
 from dotenv import load_dotenv
+from flask import Flask
 from auth import auth_bp
 from user import user_bp
 from extensions import db, mail
 
 load_dotenv()
 
-app = Flask(__name__, static_folder='static')
+app=Flask(__name__, static_folder='static')
 
-app.config['SECRET_KEY'] = os.getenv('APP_SECRET_KEY')
+app.config['SECRET_KEY']=os.getenv('APP_SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DB_URL')
-app.config['MAIL_SERVER'] = "smtp.fastmail.com"
-app.config['MAIL_PORT'] = 465
-app.config['MAIL_USERNAME'] = os.getenv('EMAIL_USERNAME')
-app.config['MAIL_PASSWORD'] = os.getenv('EMAIL_PASSWORD')
-app.config['MAIL_USE_TLS'] = False
-app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_SERVER']="smtp.fastmail.com"
+app.config['MAIL_PORT']=465
+app.config['MAIL_USERNAME']=os.getenv('EMAIL_USERNAME')
+app.config['MAIL_PASSWORD']=os.getenv('EMAIL_PASSWORD')
+app.config['MAIL_USE_TLS']=False
+app.config['MAIL_USE_SSL']=True
 app.config['MAIL_DEFAULT_SENDER'] = os.getenv('EMAIL_USERNAME')
 
 db.init_app(app)
 mail.init_app(app)
 
-app.register_blueprint(auth_bp, secret_key=app.config['SECRET_KEY'],database_url=app.config['SQLALCHEMY_DATABASE_URI'],mail_server=app.config['MAIL_SERVER'],mail_port=app.config['MAIL_PORT'],mail_username=app.config['MAIL_USERNAME'],mail_password=app.config['MAIL_PASSWORD'],use_tls=app.config['MAIL_USE_TLS'],use_ssl=app.config['MAIL_USE_SSL'],default_sender=app.config['MAIL_DEFAULT_SENDER'])
-app.register_blueprint(user_bp, secret_key=app.config['SECRET_KEY'], database_url=app.config['SQLALCHEMY_DATABASE_URI'])
+app.register_blueprint(auth_bp,\
+                        secret_key=app.config['SECRET_KEY'],\
+                        database_url=app.config['SQLALCHEMY_DATABASE_URI'],\
+                        mail_server=app.config['MAIL_SERVER'],\
+                        mail_port=app.config['MAIL_PORT'],\
+                        mail_username=app.config['MAIL_USERNAME'],\
+                        mail_password=app.config['MAIL_PASSWORD'],\
+                        use_tls=app.config['MAIL_USE_TLS'],\
+                        use_ssl=app.config['MAIL_USE_SSL'],\
+                        default_sender=app.config['MAIL_DEFAULT_SENDER'])
+app.register_blueprint(user_bp,\
+                       secret_key=app.config['SECRET_KEY'],\
+                       database_url=app.config['SQLALCHEMY_DATABASE_URI'])
 
-if __name__ == "__main__":
+if __name__=="__main__":
     app.run(debug=True)
