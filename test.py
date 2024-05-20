8bb0169c74c2bba67542c690211f3f184d59abce
alexplayer15 <alexplayer15@icloud.com>
2024-05-20 12:43:28 +0100

    new unit test

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
-import flask 
+from packaging import version
+import importlib.metadata
 
 def test_python_version():
-
-    assert flask.__version__ == '3.x'
+    flask_version = importlib.metadata.version("flask")
+    print(f'Flask version is {flask_version}')
+    assert version.parse(flask_version) >= version.parse("3.0.0")
+    print(f'Version parsed is {version.parse(flask_version)}')
