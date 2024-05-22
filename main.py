7c378150bbaa1af8fc1cd0a793694986ff1fb1cb
alexplayer15 <alexplayer15@icloud.com>
2024-05-22 09:58:11 +0100

    flask port

--- main.py
+++ main.py
@@ -39,5 +39,5 @@ app.register_blueprint(user_bp,\
                        secret_key=app.config['SECRET_KEY'],\
                        database_url=app.config['SQLALCHEMY_DATABASE_URI'])
 
-if __name__=="__main__":
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5001, debug=True)
